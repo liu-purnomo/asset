fda95f3cdefc1b04483322f4c7ac6383bdad2250
Liu Purnomo <96506259+liu-purnomo@users.noreply.github.com>
2022-11-20 05:36:29 +0700

    menambahkan folder rpa

---
+++ rpa/footer.php
@@ -0,0 +1,88 @@
+
+      <section class="bg-dark pt-5 pb-4 light">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4">
+              <p class="text-500"><b>Remote Pilot Academy</b> adalah pusat Pelatihan dan Sertifikasi Remote Pilot di Indonesia
+                <br>
+                <br> Email : <a class="text-danger" href="mailto:repil@drone.co.id">repil@drone.co.id</a>
+                <br> Phone : <a class="text-danger" href="tel:+62811319191">081-131-9191</a>
+              </p>
+              <div class="icon-group mt-4">
+                <a class="icon-item bg-white text-facebook" href="#!"><span class="bi bi-facebook"></span></a>
+                <a class="icon-item bg-white text-twitter" href="#!"><span class="bi bi-twitter"></span></a>
+                <a class="icon-item bg-white text-google-plus" href="#!"><span class="bi bi-google"></span></a>
+                <a class="icon-item bg-white text-linkedin" href="#!"><span class="bi bi-github"></span></a>
+                <a class="icon-item bg-white" href="#!"><span class="bi bi-instagram"></span></a>
+              </div>
+            </div>
+            <div class="col ps-lg-6 ps-xl-8">
+              <div class="row mt-5 mt-lg-0">
+                <div class="col-6 col-md-4">
+                  <h5 class="text-uppercase text-white opacity-85 mb-2">Pages</h5>
+                  <ul class="list-unstyled">
+                    <li class="mb-1"><a class="link-500" href="/about">About Us</a></li>
+                    <li class="mb-1"><a class="link-500" href="/contact">Contact</a></li>
+                    <li class="mb-1"><a class="link-500" href="/term">Terms</a></li>
+                    <li class="mb-1"><a class="link-500" href="/privacy">Privacy</a></li>
+                    <li class="mb-1"><a class="link-500" href="/media-kit">Media Kit</a></li>
+                  </ul>
+                </div>
+                <div class="col-6 col-md-4">
+                  <h5 class="text-uppercase text-white opacity-85 mb-2">Admin</h5>
+                  <ul class="list-unstyled">
+                    <li class="mb-1"><a class="link-500" href="/blog">Login</a></li>
+                    <li class="mb-1"><a class="link-500" href="/product">List Remote Pilot</a></li>
+                    <li class="mb-1"><a class="link-500" href="/training">List Sertifikasi</a></li>
+                    <li class="mb-1"><a class="link-500" href="/pricing">Cetak Kartu Anggota</a></li>
+                  </ul>
+                </div>
+                <div class="col mt-4 mt-md-0">
+                  <div class="row">
+                    <div class="col-6 col-md-12">
+                      <h5 class="text-uppercase text-white opacity-85 mb-2">Group Diskusi</h5>
+                      <ul class="list-unstyled">
+                        <li class="mb-1"><a class="link-500" href="https://web.facebook.com/groups/diskusidrone">Facebook</a></li>
+                        <li class="mb-1"><a class="link-500" href="https://t.me/droneindonesia">Telegram</a></li>
+                        <li class="mb-1"><a class="link-500" href="https://discord.gg/uFgUQzsqmG">Discord</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- end of .container-->
+      </section>
+      <section class="py-0 bg-dark light">
+
+        <div>
+          <hr class="my-0 text-600 opacity-25" />
+          <div class="container py-3">
+            <div class="col-12 col-sm-auto text-center">
+              <p class="mb-0 text-600 opacity-85"> &copy; 2022 <a class="text-white opacity-85" href="https://remotepilot.id"><b> Remote Pilot Academy.</b></a><span class="mb-0 text-600 opacity-85"> all rights reserved</span></p>
+            </div>
+          </div>
+        </div> 
+        <!-- end of .container-->
+      
+      </section>
+
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/gh/liu-purnomo/asset/vendors/popper/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/liu-purnomo/asset/vendors/bootstrap/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/liu-purnomo/asset/vendors/anchorjs/anchor.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/liu-purnomo/asset/vendors/is/is.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/liu-purnomo/asset/vendors/swiper/swiper-bundle.min.js"> </script>
+    <script src="https://cdn.jsdelivr.net/gh/liu-purnomo/asset/vendors/typed.js/typed.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/liu-purnomo/asset/vendors/fontawesome/all.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/liu-purnomo/asset/vendors/lodash/lodash.min.js"></script>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+    <script src="https://cdn.jsdelivr.net/gh/liu-purnomo/asset/vendors/list.js/list.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/liu-purnomo/asset/js/theme.js"></script>
+
+  </body>
+
+</html>
